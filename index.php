2c56fa86644fe8b12a242c5d91f5b0ff1ae0f2bd
1Rochass <rochass@mail.ru>
2020-01-12 15:41:07 +0300

    Added userProfile.php

--- index.php
+++ index.php
@@ -1,14 +1,26 @@
+<?php 
+session_start();
+?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title></title>
 </head>
 <body>
+
 <?php if (isset($_GET['error'])) {
 	echo "<div style='color:red'>" . $_GET['error'] ."</div><br>"; }
 ?>	
 <a href="login.php">Login</a>
 <br>
 <a href="registration.php">Registration</a>
+
+<?php
+if (isset($_SESSION['user_login'])) {
+	echo "<br>";
+	echo "<a href='userProfile.php'>Profie</a>";	
+}
+
+?>
 </body>
 </html>
